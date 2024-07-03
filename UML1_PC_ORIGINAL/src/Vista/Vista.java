d1ab61efa500d03869130b19f51fd0b5694f2833
CardeñaAdilson <U22313069@utp.edu.pe>
2024-07-02 19:09:24 -0500

    Vista

---
+++ UML1_PC_ORIGINAL/src/Vista/Vista.java
@@ -0,0 +1,14 @@
+package Vista;
+public class Vista {
+    
+    public void mostrarMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+    
+    public void  mostrarMenu(){
+        System.out.println("1. Opcion 1");
+        System.out.println("2. Opcion 2");
+        System.out.println("3. Salir");
+    }
+    
+}
