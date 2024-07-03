005185d9b8f2656df4990ed3e72a65f4da4f8d6a
CardeñaAdilson <U22313069@utp.edu.pe>
2024-07-03 08:29:12 -0500

    ClienteCuentaBancaria Avanze...Actualizado...

--- ClienteCuentaBancaria/src/Paquete1/Main.java
+++ ClienteCuentaBancaria/src/Paquete1/Main.java
@@ -2,15 +2,25 @@ package Paquete1;
 public class Main {
     public static void main(String[] args) throws Exception {
         
-        CuentaBancaria cuenta1 = new CuentaBancaria("5245705435", "01/01/2020", 1500f);
-        CuentaBancaria cuenta2 = new CuentaBancaria("5248801435", "07/05/2022", 3500f);
-        CuentaBancaria cuenta3 = new CuentaBancaria("5248444435", "07/05/2024", 4000f);
+        //CuentaBancaria cuenta1 = new CuentaBancaria("5245705435", "01/01/2020", 1500f);
+        //CuentaBancaria cuenta2 = new CuentaBancaria("5248801435", "07/05/2022", 3500f);
+        //CuentaBancaria cuenta3 = new CuentaBancaria("5248444435", "07/05/2024", 4000f);
         
         Cliente cliente = new Cliente("40508973", "Perez", "Juan", "juan.perez@gmail.com", "Calle Las Flores 123", "01/01/1990");
         
-        cliente.AgregarCuenta(cuenta1);
-        cliente.AgregarCuenta(cuenta2);
-        cliente.AgregarCuenta(cuenta3);
+        try {
+            CuentaBancaria cuenta1 = new CuentaBancaria("001", "01/01/2020", 1500.0f);
+            CuentaBancaria cuenta2 = new CuentaBancaria("002", "01/02/2020", 2000.0f);
+            
+            cliente.AgregarCuenta(cuenta1);
+            cliente.AgregarCuenta(cuenta2);
+            cliente.AgregarCuenta(cuenta2);
+            cliente.AgregarCuenta(cuenta2);
+            cliente.AgregarCuenta(cuenta2);
+            cliente.AgregarCuenta(cuenta2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
         cliente.MostrarCuentas();
     }
