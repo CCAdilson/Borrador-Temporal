a3e49e0845435f9edf3f3fc848cb6e15fcb05ed6
CardeñaAdilson <U22313069@utp.edu.pe>
2024-07-02 19:10:52 -0500

    Apuntes - Teoria

---
+++ UML1_PC_ORIGINAL/src/Teoria/Teoria.java
@@ -0,0 +1,16 @@
+package Teoria;
+public class Teoria {
+    
+    /*
+    LectorDatos: Proporciona métodos para leer diferentes tipos de entrada del
+    usuario y manejar excepciones comunes como InputMismatchException.
+    
+    Vista: Encargada de mostrar mensajes y menús al usuario, separando la lógica de presentación de la lógica de negocio.
+     
+    Controlador: Actúa como intermediario entre LectorDatos y Vista. Coordina las acciones según las entradas del usuario
+    y llama a métodos en Vista para mostrar resultados.
+    
+    App: Clase principal que inicializa los componentes del MVC y empieza la interacción con el usuario.
+    */
+    
+}
